da2af22f3b4d34f4ee05f0bfd23fe14e4c672dd4
Felipe Cardozo <dafelcardozo@gmail.com>
2016-06-24 17:13:54 -0500

    Added several gulp tasks. Loading time much improved.

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,30 @@ var minify = require('gulp-minify-css');
 var rename = require("gulp-rename");
 
 gulp.task('js', function(){
-   gulp.src('public/js/jquery/dist/jquery.js')
-   .pipe(concat('public/js/bootstrap/dist/js/bootstrap.js'))
-   .pipe(concat('public/js/angular/angular.js'))
-   .pipe(concat('public/js/angular-animate/angular-animate.js'))
-   .pipe(concat('public/js/angular-bootstrap/ui-bootstrap.js'))
-   .pipe(concat('public/js/angular-bootstrap/ui-bootstrap-tpls.js'))
-   .pipe(concat('public/js/moment/min/moment.js'))
-   .pipe(concat('public/js/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.js'))
-   .pipe(uglify())
-   .pipe(rename("build.js"))
-   .pipe(gulp.dest('public/build/js'));
+   gulp.src(['./public/js/jquery/dist/jquery.js', './public/js/bootstrap/dist/js/bootstrap.js', './public/js/angular/angular.js',
+   './public/js/angular-animate/angular-animate.js','./public/js/angular-bootstrap/ui-bootstrap.js','./public/js/angular-bootstrap/ui-bootstrap-tpls.js',
+   './public/js/moment/min/moment.min.js','./public/js/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js'
+ ])
+  .pipe(concat('build.js', {newLine: ';'}))
+  .pipe(uglify())
+  .pipe(gulp.dest('./public/build/js'));
 });
 
 gulp.task('css', function(){
-   gulp.src('public/startbootstrap-freelancer-1.0.5/css/bootstrap.css')
-   .pipe(concat('public/js/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css'))
-   .pipe(concat('public/js/angular-bootstrap/ui-bootstrap-csp.css'))
-   .pipe(concat('public/startbootstrap-freelancer-1.0.5/css/freelancer.css'))
-   .pipe(concat('public/startbootstrap-freelancer-1.0.5/font-awesome/css/font-awesome.css'))
+   gulp.src(['./public/startbootstrap-freelancer-1.0.5/css/bootstrap.css', './public/js/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+ './public/js/angular-bootstrap/ui-bootstrap-csp.css', './public/startbootstrap-freelancer-1.0.5/css/freelancer.css', './public/startbootstrap-freelancer-1.0.5/font-awesome/css/font-awesome.css'])
+   .pipe(concat('build.css', {newLine: ';'}))
    .pipe(minify())
-   .pipe(rename('build.css'))
-   .pipe(gulp.dest('public/build/css'));
+//   .pipe(rename('build.css'))
+   .pipe(gulp.dest('./public/build/css'));
 });
 
 gulp.task('copyfonts', function() {
-   gulp.src('public/js/bootstrap/fonts/*.{ttf,woff,eof,svg}')
-   .pipe(gulp.dest('public/build/fonts'));
-   gulp.src('public/startbootstrap-freelancer-1.0.5/font-awesome/fonts/*.{ttf,woff,eof,svg}')
-   .pipe(gulp.dest('public/build/fonts'));
+   gulp.src('./public/js/bootstrap/fonts/*.{ttf,woff,woff2,eof,svg}')
+   .pipe(gulp.dest('./public/build/fonts'));
+   gulp.src('./public/startbootstrap-freelancer-1.0.5/font-awesome/fonts/*.{ttf,woff,woff2,eof,svg}')
+   .pipe(gulp.dest('./public/build/fonts'));
 });
 
-gulp.task('default',['js','css'],function(){
+gulp.task('default',['js','css', 'copyfonts'],function(){
 });
